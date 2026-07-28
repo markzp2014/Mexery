66e99b8805f6abce100f810b902b8425331d9032
markzp2014 <mark@magnusega.com>
2026-07-28 17:55:35 +0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,1 +1,37 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
 
+function resize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+window.addEventListener("resize", resize);
+resize();
+
+function update() {
+
+}
+
+function draw() {
+    ctx.fillStyle = "#111";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = "white";
+    ctx.font = "32px monospace";
+    ctx.textAlign = "center";
+
+    ctx.fillText(
+        "Mexery Engine v0.1",
+        canvas.width / 2,
+        canvas.height / 2
+    );
+}
+
+function gameLoop() {
+    update();
+    draw();
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
